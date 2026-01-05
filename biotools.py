03cae7172d5f0643c467750406a671c57019a251
Mazuzu33 <mazuki.wu@gmail.com>
2026-01-04 20:01:16 -0600

    Update names of functions

--- biotools.py
+++ biotools.py
@@ -1,4 +1,4 @@
-def meltingTemperature(strand):
+def calcMeltTemp(strand):
     # Count the number of different nucleotides
     numA = 0
     numT = 0
@@ -20,7 +20,7 @@ def meltingTemperature(strand):
     else:
         meltTemp = 64.9 + 41 * (numG + numC - 16.4) / (numA + numT + numG + numC)
 
-def gcContent(strand):
+def calcGCContent(strand):
     # Count the number of G's and C's
     numG = 0
     numC = 0
@@ -32,6 +32,7 @@ def gcContent(strand):
             numC += 1
         numNuc += 1
     return (numG + numC) / numNuc * 100
+
     
 
 
