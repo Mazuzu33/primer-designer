6cec3f46f874fe3882a769df5f69934dd92a4522
Mazuzu33 <mazuki.wu@gmail.com>
2026-01-04 19:43:45 -0600

    Add function to calculate melting temperature

---
+++ biotools.py
@@ -0,0 +1,25 @@
+def meltingTemperature(strand):
+    # Count the number of different nucleotides
+    numA = 0
+    numT = 0
+    numG = 0
+    numC = 0
+    for letter in strand:
+        if (letter == "A"):
+            numA += 1
+        elif (letter == "T"):
+            numT += 1
+        elif (letter == "G"):
+            numG += 1
+        else:
+            numC += 1
+    numNuc = numA + numT + numG + numC
+    # Calculate the melting temperature differently based on the size of the strand
+    if (numNuc <= 13):
+        meltTemp = (numA + numT) * 2 + (numG + numC) * 4
+    else:
+        meltTemp = 64.9 + 41 * (numG + numC - 16.4) / (numA + numT + numG + numC)
+
+
+
+
