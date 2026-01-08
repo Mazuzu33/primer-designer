465b96be20826d19d14cbb467b256bd03e4191b5
Mazuzu33 <mazuki.wu@gmail.com>
2026-01-08 17:32:37 -0600

    Add suffix trie datastructure

--- datastructures.py
+++ datastructures.py
@@ -32,5 +32,70 @@ class Primer:
     gcContent: float
     gcInClamp: int
 
+class Node:
+    '''
+    A generic trie node
+    '''
+    def __init__(self):
+        self.position = None
+        self.nextNodes = {}
+    
+    def getPosition(self):
+        return self.position
+    
+    def getNextNode(self, key):
+        return self.nextNodes.get(key)
+    
+    def getNextNodes(self):
+        return self.nextNodes
+    
+    def setPosition(self, position):
+        self.position = position
+    
+    def addNextNode(self, key, node):
+        self.nextNodes[key] = node
+
+    
+class SuffixTrie:
+    '''
+    A generic suffix trie datastructure
+    '''
+    def __init__(self):
+        self.root = Node()
+        self.edges = 0
+
+    def addWord(self, word, position):
+        curNode = self.root
+        # Loop through all the letters of the word
+        for j in range(len(word)):
+            # Check if the letter is already a key for a next node, if not, create a next node with the letter as the key.
+            if (curNode.getNextNode(word[j]) == None):
+                curNode.addNextNode(word[j], Node())
+                self.edges += 1
+            curNode = curNode.getNextNode(word[j])
+        # Set the position of the last node indicating where the word can be found
+        curNode.setPosition(position)
+    
+    def addSuffixes(self, word):
+        #Add each suffix to the trie
+        for i in range(len(word)):
+            suffix = word[i:]
+            self.addWord(suffix, i)
+    
+    def getEdges(self):
+        return self.edges
+    
+    # Debugger method to print out paths of the trie
+    def printPathsRoot(self):
+        self.printPaths(self.root, "")
+    
+    # Helper method for printPathsRoot
+    def printPaths(self, node, word):
+        # Base case
+        if len(node.getNextNodes()) == 0:
+            print(word)
+        for key in node.getNextNodes().keys():
+            self.printPaths(node.getNextNode(key), word + key)
 
 
+    
